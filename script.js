8d95b9dbfe41f77c82a9146aa4a8a0eb127dce8c
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-12 09:56:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,25 @@
-window.promises = [];
+window.promises = [
+	new Promise(resolve => setTimeout(() => resolve('Promise 1'), getRandomTime())),
+  new Promise(resolve => setTimeout(() => resolve('Promise 2'), getRandomTime())),
+  new Promise(resolve => setTimeout(() => resolve('Promise 3'), getRandomTime())),
+  new Promise(resolve => setTimeout(() => resolve('Promise 4'), getRandomTime())),
+  new Promise(resolve => setTimeout(() => resolve('Promise 5'), getRandomTime()))
+
+];
 
 // Do not change the code above this
 // add your promises to the array `promises`
 
-let promise1 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise1");
-    }, Math.floor(Math.random() * 5000) + 1000);
-});
+Promise.any(promises)
+  .then(result => {
+    const outputDiv = document.getElementById('output');
+    outputDiv.textContent = result;
+  })
+  .catch(error => {
+    console.log('Error:', error);
+  });
 
-let promise2 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise2");
-    }, Math.floor(Math.random() * 5000) + 1000);
-});
-
-let promise3 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise3");
-    }, Math.floor(Math.random() * 5000) + 1000);
-});
-
-let promise4 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise4");
-    }, Math.floor(Math.random() * 5000) + 1000);
-});
-
-let promise5 = new Promise((resolve, reject) => {
-    setTimeout(() =>{
-        resolve("Promise5");
-    }, Math.floor(Math.random() * 5000) + 1000);
-});
-
-const myArray = [promise1, promise2, promise3, promise4, promise5];
-
-Promise.any(myArray).then((result) => {
-    document.getElementById("output").innerText = result;
-});
+// Helper function to generate random time between 1 and 5 seconds
+function getRandomTime() {
+  return Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
+}

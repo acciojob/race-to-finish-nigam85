b7e834ed4d5b670e8a75f6b2ef3e3c0734422265
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-09-12 09:54:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,7 @@
 window.promises = [];
 
-// Function to generate a random delay between 1 and 5 seconds
-function getRandomDelay() {
-  return Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds
-}
-
-// Array of promises that resolve with random delays
-const promises = [
-  new Promise((resolve) => setTimeout(() => resolve("Promise 1 resolved!"), getRandomDelay())),
-  new Promise((resolve) => setTimeout(() => resolve("Promise 2 resolved!"), getRandomDelay())),
-  new Promise((resolve) => setTimeout(() => resolve("Promise 3 resolved!"), getRandomDelay())),
-  new Promise((resolve) => setTimeout(() => resolve("Promise 4 resolved!"), getRandomDelay())),
-  new Promise((resolve) => setTimeout(() => resolve("Promise 5 resolved!"), getRandomDelay())),
-];
-
-// Using Promise.any() to wait for the first resolved promise
-Promise.any(promises)
-  .then((result) => {
-    const outputDiv = document.getElementById("output");
-    outputDiv.innerText = result;
-  })
-  .catch((error) => {
-    console.log("Error occurred:", error);
-  });
+// Do not change the code above this
+// add your promises to the array `promises`
 
 let promise1 = new Promise((resolve, reject) => {
     setTimeout(() =>{
@@ -59,6 +38,3 @@ const myArray = [promise1, promise2, promise3, promise4, promise5];
 Promise.any(myArray).then((result) => {
     document.getElementById("output").innerText = result;
 });
-
-// Do not change the code above this
-// add your promises to the array `promises`
